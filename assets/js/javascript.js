5023e45ed7c13f551e72a3edd3cb708f34f71dda
zDenisS <denis_st2007@hotmail.com>
2019-10-21 09:17:03 -0300

    Implementado função de Cadastrar, Sair e adicionado novas paginas

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -17,3 +17,42 @@ function Logar(){
     document.getElementById("botao-cadastrar").style.visibility = "hidden";
 
 }
+
+function Cadastrar(){
+    var nome = document.getElementById("Cnome").value;
+    var email = document.getElementById("Cemail").value;
+    var senha = document.getElementById("Csenha").value;
+    var data = document.getElementById("Cdata").value;    
+
+    document.getElementById("UCnome").innerHTML = nome;
+    document.getElementById("UCemail").innerHTML = email;
+    document.getElementById("UCsenha").value = senha;
+    document.getElementById("labelteste").innerHTML = data;
+}
+
+function Sair(){
+    var avatar1 = "assets/img/avatar0.png";
+    var conectado = "Desconectado";
+    var email = "...";
+    var imagem = document.getElementsByClassName("mg-avatar1");
+    document.getElementById("status-email").innerHTML = email;
+    document.getElementById("status-nome").innerHTML = "...";
+    document.getElementById("status").innerHTML = conectado;
+
+    document.getElementById("img-avatar").src = avatar1;
+    document.getElementById("img-avatar1").src = avatar1;
+
+    document.getElementById("div-avatar").style.marginTop = "0";
+    document.getElementById("botao-sair").style.visibility = "hidden";
+    document.getElementById("img-avatar1").style.visibility= "hidden";
+    document.getElementById("botao-login").style.visibility = "visible";
+    document.getElementById("botao-cadastrar").style.visibility = "visible";
+}
+
+function Sair2(){
+
+    document.getElementById("botao-sair").style.visibility = "hidden";
+    document.getElementById("img-avatar1").style.visibility= "hidden";
+    document.getElementById("botao-login").style.visibility = "visible";
+    document.getElementById("botao-cadastrar").style.visibility = "visible";
+}
